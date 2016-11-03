8248bd1702952af9ec53d88179aa00808c462662
Mallikarjuna <drop2ms@gmail.com>
2016-11-04 00:03:03 +0530

    Added Java Doc Comments

--- src/main/java/com/worldcountries/controller/CountryController.java
+++ src/main/java/com/worldcountries/controller/CountryController.java
@@ -14,20 +14,40 @@ import java.util.List;
 
 @Controller
 public class CountryController {
+    /**
+     * @Autowired Indicates the Dependence Injection of countries Repository
+    */
     @Autowired
     CountriesRepository countriesRepository;
+    /**
+     * Maps to User URI Request : http://localhost:8080/
+     * @param modelMap is POJO Object Provided to  VIEW
+     * @return index VIEW list all the countries in Repository
+     */
     @RequestMapping("/")
     public String listCountries(ModelMap modelMap) {
         List<Country> worldCountries = countriesRepository.getWorldCountries();
         modelMap.put("countries",worldCountries);
         return "index";
     }
+    /**
+     * Maps to User URI Request: http://localhost:8080/country/Ecuador
+     * @param name holds the country name
+     * @param modelMap is POJO Object Provided to VIEW
+     * @return country VIEW detailed view of Country selected by User
+     */
     @RequestMapping("/country/{name}")
     public String country(@PathVariable String name,ModelMap modelMap) {
         Country country = countriesRepository.findCountryByName(name);
         modelMap.put("country",country);
         return "country";
     }
+    /**
+     * Sorts Countries by Name
+     * Maps to User URI Request: http://localhost:8080/sort/country/name
+     * @param modelMap is POJO Object Provided to VIEW
+     * @return index VIEW displays sorted country list
+     */
     @RequestMapping("/sort/country/name")
     public String countriesByName(ModelMap modelMap) {
         List<Country> sortedCountriesName = countriesRepository.getWorldCountries();
@@ -35,6 +55,12 @@ public class CountryController {
         modelMap.put("countries",sortedCountriesName);
         return "index";
     }
+    /**
+     * Sorts Countries by Population
+     * Maps to User URI Request : http://localhost:8080/sort/country/population
+     * @param modelMap is POJO Object Provided to VIEW
+     * @return index VIEW display sorted country list
+     */
     @RequestMapping("/sort/country/population")
     public String countriesByPopulation(ModelMap modelMap) {
         List<Country> sortedCountriesPopulation = countriesRepository.getWorldCountries();
