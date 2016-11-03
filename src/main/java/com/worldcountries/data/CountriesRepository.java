e60401cf76a639601556dd86557564ed29ef6115
Mallikarjuna <drop2ms@gmail.com>
2016-11-03 23:43:47 +0530

    Uniquely Identify Detailed View of Countries

--- src/main/java/com/worldcountries/data/CountriesRepository.java
+++ src/main/java/com/worldcountries/data/CountriesRepository.java
@@ -4,6 +4,7 @@ import com.worldcountries.model.Country;
 import com.worldcountries.Exception.NotFoundException;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,7 +12,7 @@ import java.util.List;
 public class CountriesRepository {
     private static final List<Country> WORLD_COUNTRIES = Arrays.asList(
             new Country("India",1293057000,"Delhi", Arrays.asList("Hindi","English"),"india"),
-            new Country("Kenya",45010056,"Nairobi", Arrays.asList("Kiswahili","English"),"kenya"),
+            new Country("India",45010056,"Nairobi", Arrays.asList("Kiswahili","English"),"kenya"),
             new Country("Ecuador",16144000,"Quito", Arrays.asList("Spanish"),"ecuador"),
             new Country("France",66736000,"Paris", Arrays.asList("French"),"france"),
             new Country("Italy",60674003,"Rome", Arrays.asList("Italian"),"italy")
@@ -26,7 +27,7 @@ public class CountriesRepository {
                 .filter(Country -> Country.getCountryName().equals(name))
                 .findFirst()
                 .orElseThrow(NotFoundException::new);
-    }
 
+    }
 
 }
